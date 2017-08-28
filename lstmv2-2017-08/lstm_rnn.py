6a003ebafb07a4a40b9a9691999b3a934aad359d
hongcheng <hongcheng@sohu-inc.com>
2017-08-28 19:52:27 +0800

    [hcheng] use learning decay to bidirection lstm

--- lstmv2-2017-08/lstm_rnn.py
+++ lstmv2-2017-08/lstm_rnn.py
@@ -5,8 +5,8 @@ from tensorflow.contrib.legacy_seq2seq.python.ops import seq2seq
 HIDDEN_SIZE = 512
 NUM_LAYERS = 4
 VOCAB_SIZE = 33
-LEARNING_RATE = 0.8
-TRAIN_BATCH_SIZE = 2
+LEARNING_RATE = 0.7
+TRAIN_BATCH_SIZE = 20
 TRAIN_NUM_STEP = 6
 
 EVAL_BATCH_SIZE = 1
@@ -14,14 +14,14 @@ EVAL_NUM_STEP = 6
 NUM_EPOCH = 5000
 KEEP_PROB = 0.5
 # MAX_GRAD_NORM = 5
-MAX_GRAD_NORM = 10
+MAX_GRAD_NORM = 5
 REGULARIZATION_RATE = 0.0001
 MOVING_AVERAGE_DECAY = 0.99
 LEARNING_RATE_DECAY = 0.99
 
 
 class LSTMModel(object):
-    def __init__(self, is_training, batch_size, num_steps):
+    def __init__(self, is_training, batch_size, num_steps, batch_counts):
         self.batch_size = batch_size
         self.num_steps = num_steps
         self.input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
@@ -62,21 +62,25 @@ class LSTMModel(object):
                         tf.get_variable_scope().reuse_variables()
                     cell_output_bw, state_bw = rnn_cell_bw(inputs[:, time_step, :], state_bw)
                     outputs_bw.append(cell_output_bw)
+
             outputs_bw = tf.reverse(outputs_bw, [0])
             output = tf.concat([outputs_fw, outputs_bw], 2)
-            output = tf.transpose(output, perm=[1, 0, 2])
-            output = tf.reshape(output, [-1, HIDDEN_SIZE * 2])
+            output = tf.reshape(tf.concat(output, 1), [-1, HIDDEN_SIZE * 2])
 
         weight = tf.get_variable("weight", [HIDDEN_SIZE * 2, VOCAB_SIZE])
         bias = tf.get_variable("bias", [VOCAB_SIZE])
         logits = tf.matmul(output, weight) + bias
-
         loss = seq2seq.sequence_loss_by_example([logits],
                                                 [tf.reshape(self.targets, [-1])],
-                                                [tf.ones([batch_size * num_steps], dtype=tf.float32)])
+                                                weights=[tf.ones([batch_size * num_steps], dtype=tf.float32)])
 
-        self.cost = tf.reduce_sum(loss) / batch_size
-        self.correct_prediction = tf.equal(tf.cast(tf.argmax(logits, 1), tf.int32), tf.reshape(self.targets, [-1]))
+        # self.cost = tf.reduce_sum(loss) / batch_size
+        self.cost = tf.reduce_mean(loss)
+        self.global_step = tf.Variable(0, dtype=tf.int32)
+        self.learning_rate = tf.train.exponential_decay(LEARNING_RATE, self.global_step, batch_counts / self.batch_size,
+                                                        LEARNING_RATE_DECAY, staircase=True)
+        self.predictions = tf.cast(tf.argmax(logits, 1), tf.int32)
+        self.correct_prediction = tf.equal(self.predictions, tf.reshape(self.targets, [-1]))
         self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
         self.final_state_fw = state_fw
         self.final_state_bw = state_bw
@@ -85,39 +89,52 @@ class LSTMModel(object):
         trainable_variables = tf.trainable_variables()
         grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, trainable_variables), MAX_GRAD_NORM)
 
-        optimizer = tf.train.AdamOptimizer(LEARNING_RATE)
-        self.train_op = optimizer.apply_gradients(zip(grads, trainable_variables),
-                                                  global_step=tf.train.get_or_create_global_step())
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost, global_step=self.global_step)
+        # self.train_op = optimizer.apply_gradients(zip(grads, trainable_variables),
+        #                                           global_step=tf.train.get_or_create_global_step())
 
 
-def run_epoch(session, model, data, train_op, output_log):
+def run_epoch(session, model, data, train_op, output_log, total_iter, is_training):
     total_costs = 0.0
     iters = 0
     state_fw = session.run(model.initial_state_fw)
     state_bw = session.run(model.initial_state_bw)
-    data_len = len(data)
+    data_len = len(data) // model.batch_size
     for step, (x, y) in enumerate(get_enumerate_in_array(data, model.batch_size, model.num_steps)):
-        cost, state_fw, state_bw, accuracy, _ = session.run([model.cost, model.final_state_fw, model.final_state_bw, model.accuracy, train_op],
-                                                  {
-                                                      model.input_data:x,
-                                                      model.targets:y,
-                                                      model.initial_state_fw:state_fw,
-                                                      model.initial_state_bw:state_bw
-                                                  })
+        # merged_op = tf.summary.merge_all()
+        # summary_writer = tf.summary.FileWriter("./logs/lottery_logs", session.graph)
+        cost, state_fw, state_bw, accuracy, learning_rate, predications, _ = session.run(
+            [model.cost, model.final_state_fw, model.final_state_bw,
+             model.accuracy, model.learning_rate, model.predictions, train_op],
+            {
+                model.input_data: x,
+                model.targets: y,
+                model.initial_state_fw: state_fw,
+                model.initial_state_bw: state_bw
+            })
+
         total_costs += cost
         iters += model.num_steps
         if output_log and (step == 0 or step == data_len // 5 or step == data_len * 2 // 5 or step == data_len * 3 // 5
-        or step == data_len * 4 // 5 or step == data_len - 1):
+                           or step == data_len * 4 // 5 or step == data_len - 1):
+            # if is_training:
+            #     summary_str = session.run(merged_op)
+            #     summary_writer.add_summary(summary_str, total_iter * data_len + step)
             print("After %d steps, perplexity is %.3f, cost is %0.6f" % (step, np.exp(total_costs / (iters + 0)), cost))
             print("Accuracy = %.6f" % accuracy)
+            print("Learning rate = %.10f" % learning_rate)
+            print("Predications is as following:")
+            print(predications[0:6])
+            print("Labels is as following:")
+            print(y[0:6])
     return np.exp(total_costs / (iters + 0)), accuracy
 
 
 def get_enumerate_in_array(raw_data, batch_size, num_steps):
     raw_data = np.array(raw_data)
-    data_len = len(raw_data)
-    if data_len - data_len // batch_size * batch_size <= 0:
-        raise ValueError("data len should be greater at least 1")
+    data_len = len(raw_data) - batch_size
+    # if data_len - data_len // batch_size * batch_size <= 0:
+    #    raise ValueError("data len should be greater at least 1")
     for i in range(data_len // batch_size):
         x = raw_data[i * batch_size:(i + 1) * batch_size, :]
         y = raw_data[i * batch_size + 1:(i + 1) * batch_size + 1, :]
@@ -178,23 +195,24 @@ def main():
     train_data, valid_data, test_data = get_data_in_array()
     initializer = tf.random_uniform_initializer(-0.05, 0.05)
     with tf.variable_scope("lottery_train", reuse=None, initializer=initializer):
-        train_model = LSTMModel(True, TRAIN_BATCH_SIZE, TRAIN_NUM_STEP)
+        train_model = LSTMModel(True, TRAIN_BATCH_SIZE, TRAIN_NUM_STEP, len(train_data))
 
     with tf.variable_scope("lottery_train", reuse=True, initializer=initializer):
-        eval_model = LSTMModel(False, EVAL_BATCH_SIZE, EVAL_NUM_STEP)
+        eval_model = LSTMModel(False, EVAL_BATCH_SIZE, EVAL_NUM_STEP, len(valid_data))
     saver = tf.train.Saver()
     with tf.Session() as session:
         tf.global_variables_initializer().run()
         for i in range(NUM_EPOCH):
             print("In iteration: %d" % (i + 1))
-            run_epoch(session, train_model, train_data, train_model.train_op, True)
+            run_epoch(session, train_model, train_data, train_model.train_op, True, i, True)
 
-            valid_perplexity,accuracy = run_epoch(session, eval_model, valid_data, tf.no_op(), False)
+            valid_perplexity, accuracy = run_epoch(session, eval_model, valid_data, tf.no_op(), False, i, False)
             print("Epoch: %d Validation Perplexity: %.3f" % (i + 1, valid_perplexity))
             # if i % 10 == 0:
             #    for i in range(len(train_costs)):
             #        print("loss on training is %.6f" % (train_costs[i]))
-            if (i >= 500 and i % 500 == 0) or (valid_perplexity < 10 and i % 20 == 0) or (accuracy > 0.9 and i % 20 == 0):
+            if (i >= 500 and i % 500 == 0) or (valid_perplexity < 10 and i % 20 == 0) or (
+                    accuracy > 0.9 and i % 20 == 0):
                 #    for i in range(len(valid_costs)):
                 #        print("loss on valid data is %.6f" % (valid_costs[i]))
                 saver.save(session, "./model.ckpt")
